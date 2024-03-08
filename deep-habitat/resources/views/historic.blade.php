2c9cee825f5415e0b34823c76eaa68d2d5ad8e95
MiguelGallardoByInfinitum <159464688+MiguelGallardoByInfinitum@users.noreply.github.com>
2024-03-08 15:46:39 +0100

    laravel initialization

---
+++ deep-habitat/resources/views/historic.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Historic</title>
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <h1>Hola</h1>
+    <a href="/">Click here to return to LOGIN</a>
+</body>
+</html>
---
+++ deep-habitat/resources/views/welcome.blade.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  @vite('resources/css/app.css')
+</head>
+<body class="min-h-screen flex justify-center items-center flex-col gap-5 bg-gradient-to-r from-slate-900 to-red-950 text-white">
+    <h1 class="text-2xl font-bold capitalize text-red-500">
+        Soy un chico laravel
+    </h1>
+    <?php
+        echo "<p class='text-red-600'>Y el Guillem no</p>"
+    ?>
+    <hr class="w-44">
+    <form class="flex flex-col justify-center items-center gap-3">
+        <input type="text" id="username" name="username" placeholder="Username here..." class="w-80 h-9 text-lg text-black" required>
+        <input type="password" id="password" name="password" placeholder="Password here .." class="w-80 h-9 text-lg text-black" required>
+        <input type="submit" id="login" name="login" value="Iniciar Sessión" class="mt-2 bg-rose-700 p-2 w-32 rounded-full hover:bg-rose-900 cursor-pointer transition-colors duration-300">
+    </form>
+    <a href="/historic">Click here to go to HISTORIC</a>
+</body>
+</html>
