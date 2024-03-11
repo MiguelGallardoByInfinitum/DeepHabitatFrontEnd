641d18d60d8743dda448407a9737f50b73d6652a
guillembyinfinitum <159464752+guillembyinfinitum@users.noreply.github.com>
2024-03-11 17:43:48 +0100

    .

--- deep-habitat/resources/views/welcome.blade.php
+++ deep-habitat/resources/views/welcome.blade.php
@@ -15,7 +15,7 @@
     <form action={{ url('post') }} method="post" class="flex flex-col justify-center items-center gap-3">
         @csrf
         <input type="text" id="username" name="username" placeholder="Username here..." class="w-80 h-9 text-lg text-black p-2" required>
-        <input type="password" id="password" name="password" placeholder="Password here .." class="w-80 h-9 text-lg text-black p-2" required>
+        <input type="password" id="password" name="password" placeholder="Password here..." class="w-80 h-9 text-lg text-black p-2" required>
         <input type="submit" id="login" name="login" value="Iniciar Sessión" class="mt-2 p-2 w-32 rounded-full cursor-pointer duration-300 hover:-translate-y-1 transition-all bg-primary text-secondary border-2 border-primary hover:bg-secondary hover:text-primary">
     </form>
 </body>
