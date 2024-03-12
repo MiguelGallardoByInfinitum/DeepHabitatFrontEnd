eace1a6bc62c798de8946e2dca6101a39e970236
guillembyinfinitum <159464752+guillembyinfinitum@users.noreply.github.com>
2024-03-12 15:24:42 +0100

    update

--- deep-habitat/resources/views/historic.blade.php
+++ deep-habitat/resources/views/historic.blade.php
@@ -16,29 +16,34 @@
 </head>
 <body class="selection:text-light selection:bg-primarySelection">
     <header class="flex justify-between items-center p-7">
-        <h1 class="text-4xl font-bold">HISTORIC</h1>
+        <h1 class="text-4xl font-bold text-primary">Por decidir</h1>
         <nav class="flex justify-center items-center gap-5">
-            <a href="/new" class="bg-primary text-light p-2 rounded-md drop-shadow-lg hover:bg-primary2 transition-colors"><i class='bx bx-plus translate-y-0.5'></i> New Job</a>
+            <a href="/new" class="bg-primary text-light p-2 rounded-md drop-shadow-lg hover:bg-primary2 hover:-translate-y-1 transition-all duration-300"><i class='bx bx-plus translate-y-0.5'></i> New Job</a>
             <a href="/" class="hover:-translate-y-1 transition-transform duration-300">Log Out</a>
         </nav>
     </header>
     <div class="flex justify-center items-start flex-col mr-36 ml-36 mt-10">
-        <h3 class="text-2xl">Jobs</h3>
+        <h3 class="text-2xl">HISTÓRICO</h3>
         <div class="w-[100%] jobs mt-6">
             <div class="w-auto flex justify-between m-2">
-                <p>Job 1...</p><button>Descargar CSV</button>
+                <p>Job 1...</p>
+                <button>Descargar CSV</button>
             </div>
             <div class="w-auto flex justify-between m-2">
-                <p>Job 2...</p><button>Descargar CSV</button>
+                <p>Job 2...</p>
+                <button>Descargar CSV</button>
             </div>
             <div class="w-auto flex justify-between m-2">
-                <p>Job 3...</p><button>Descargar CSV</button>
+                <p>Job 3...</p>
+                <button>Descargar CSV</button>
             </div>
             <div class="w-auto flex justify-between m-2">
-                <p>Job 4...</p><button>Descargar CSV</button>
+                <p>Job 4...</p>
+                <button>Descargar CSV</button>
             </div>
             <div class="w-auto flex justify-between m-2">
-                <p>Job 5...</p><button>Descargar CSV</button>
+                <p>Job 5...</p>
+                <button>Descargar CSV</button>
             </div>
         </div>
     </div>
--- deep-habitat/resources/views/welcome.blade.php
+++ deep-habitat/resources/views/welcome.blade.php
@@ -8,15 +8,15 @@
 </head>
 <body class="min-h-screen flex justify-center items-center flex-col gap-5 selection:bg-red-400 selection:text-light selection:bg-primarySelection bg-secondary">
     <h1 class="text-3xl font-bold capitalize drop-shadow-md shadow-red-200 text-primary">
-        Iniciar Session
+    Sign in to your account
     </h1>
     <p class='text-1xl text-tertiary'>DeepHabitat API</p>
     <hr class="w-44 text-primary">
     <form action={{ url('post') }} method="post" class="flex flex-col justify-center items-center gap-3">
         @csrf
-        <input type="text" id="username" name="username" placeholder="Username here..." class="w-80 h-9 text-lg text-primary p-2 bg-secondary rounded-lg drop-shadow-md focus:outline-none" required>
-        <input type="password" id="password" name="password" placeholder="Password here..." class="w-80 h-9 text-lg text-primary p-2 bg-secondary rounded-lg drop-shadow-md focus:outline-none" required>
-        <input type="submit" id="login" name="login" value="Iniciar Sessión" class="mt-2 p-2 w-32 rounded-lg cursor-pointer duration-300 hover:-translate-y-1 transition-all bg-primary text-secondary border-2 border-primary hover:bg-secondary hover:text-primary drop-shadow-md">
+        <input type="text" id="username" name="username" placeholder="Username here..." class="w-80 h-9 text-md text-primary p-2 bg-secondary rounded-lg drop-shadow-md focus:outline-none" required>
+        <input type="password" id="password" name="password" placeholder="Password here..." class="w-80 h-9 text-md text-primary p-2 bg-secondary rounded-lg drop-shadow-md focus:outline-none" required>
+        <input type="submit" id="login" name="login" value="Log In" class="mt-3 p-1 w-32 rounded-lg cursor-pointer duration-300 hover:-translate-y-1 transition-all bg-primary text-secondary border-2 border-primary hover:bg-secondary hover:text-primary drop-shadow-md">
     </form>
 </body>
 </html>
