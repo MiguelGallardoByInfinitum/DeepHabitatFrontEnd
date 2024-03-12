b2e075419ac51741d1aa16b99a9d4fc78119a606
guillembyinfinitum <159464752+guillembyinfinitum@users.noreply.github.com>
2024-03-12 17:48:26 +0100

    update del new

--- deep-habitat/resources/views/new.blade.php
+++ deep-habitat/resources/views/new.blade.php
@@ -3,12 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
     <title>New Job</title>
     @vite('resources/css/app.css')
 </head>
 <body class="overflow-hidden">
     <h1>NEW JOB</h1>
-    <a href="/historic">Click to return to HISTORIC</a>
-    <h1 class="text-primary text-[50em] absolute -top-[20%] left-[40%] p-0 m-0">6</h1>
+    <a href="/historic"><i class='bx bx-no-entry' ></i> Cancel</a>
+    <!-- <h1 class="text-primary text-[50em] absolute -top-[20%] left-[40%] p-0 m-0">6</h1> -->
 </body>
 </html>
