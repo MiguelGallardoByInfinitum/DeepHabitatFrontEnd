fb4482e2973aee0b36775e6585e43544f0dced0a
guillembyinfinitum <159464752+guillembyinfinitum@users.noreply.github.com>
2024-03-18 16:37:39 +0100

    fetch origin

--- deep-habitat/resources/views/jobs.blade.php
+++ deep-habitat/resources/views/jobs.blade.php
@@ -18,7 +18,7 @@ if(!Session::has('username')) {
 <body class="selection:text-light selection:bg-primarySelection">
     <header class="flex justify-between items-center p-7">
         <div class="flex gap-4">
-        <img src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" alt="User-Icon" class="w-10 h-10 hue-rotate-90 grayscale-[20%] hidden md:block">
+        <img src="https://cdn-icons-png.flaticon.com/512/3177/3177440.png" alt="User-Icon" class="w-10 h-10 hue-rotate-90 grayscale-[20%] hidden md:block pointer-events-none select-none">
             <h1 class=" text-2xl md:text-4xl font-bold text-primary">{{ Session::get('username') }}</h1>
         </div>
         <nav class="flex justify-center items-center gap-5">
