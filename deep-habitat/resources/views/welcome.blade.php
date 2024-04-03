6ce50962a25827edebaa2cbda31d31701eb245aa
guillembyinfinitum <159464752+guillembyinfinitum@users.noreply.github.com>
2024-04-03 15:55:00 +0200

    añadido logo

--- deep-habitat/resources/views/welcome.blade.php
+++ deep-habitat/resources/views/welcome.blade.php
@@ -7,8 +7,8 @@
   <title>Login</title>
   @vite('resources/css/app.css')
 </head>
-<body class="min-h-screen flex justify-center items-center flex-col gap-5 selection:bg-red-400 selection:text-light selection:bg-primarySelection bg-secondary">
-    <h1 class="text-3xl font-bold capitalize drop-shadow-md shadow-red-200 text-primary" data-aos="zoom-in-up" data-aos-delay="100">
+<body class="min-h-screen flex justify-center items-center flex-col gap-5 selection:bg-red-400 selection:text-light selection:bg-primarySelection bg-secondary">    
+<h1 class="text-3xl font-bold capitalize drop-shadow-md shadow-red-200 text-primary" data-aos="zoom-in-up" data-aos-delay="100">
     Sign in to your account
     </h1>
     <p class='text-1xl text-dark' data-aos="zoom-in-up" data-aos-delay="200">DeepHabitat API</p>
@@ -21,7 +21,8 @@
         <input type="text" id="username" name="username" placeholder="Username here..." class="w-80 h-9 text-md text-primary p-2 bg-secondary rounded-lg drop-shadow-md focus:outline-none" required>
         <input type="password" id="password" name="password" placeholder="Password here..." class="w-80 h-9 text-md text-primary p-2 bg-secondary rounded-lg drop-shadow-md focus:outline-none" required>
         <input type="submit" id="login" name="login" value="Log In" class="mt-3 p-1 w-32 rounded-lg cursor-pointer duration-300 hover:-translate-y-1 transition-all bg-primary text-secondary border-2 border-primary hover:bg-secondary hover:text-primary drop-shadow-md select-none">
-    </form>
+      </form>
+      <img src="{{ asset('media/DeepHABITAT Logo Black.png') }}" alt="DeepHABITAT Logo" class="w-24 absolute md:top-5 md:left-5 top-10 cursor-pointer transition-all duration-300 opacity-30 hover:opacity-100">
 
     <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
     <script>
